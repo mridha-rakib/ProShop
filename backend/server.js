014022da2d781d4f67c6cd634f3f6318137563e0
RAKIB <rakibmahmudmridha@gmail.com>
2024-01-15 19:32:21 +0600

    backend user authenication complete

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 dotenv.config();
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
@@ -16,12 +17,17 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// cookie parser
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
+
+//
 
 // Err
 app.use(notFound);
