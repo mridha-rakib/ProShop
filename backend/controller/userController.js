66e7e3a96b75f7361a5fe40591ada818201cf6e8
RAKIB <rakibmahmudmridha@gmail.com>
2024-01-15 07:32:49 +0600

    backend route controller setup

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -28,3 +28,50 @@ const logoutUser = asyncHandler(async (req, res) => {
 const getUserProfile = asyncHandler(async (req, res) => {
   res.send("get  user profile");
 });
+
+// @desc get user profile
+// @route PUT /api/users/profile
+// @access private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.send("update  user profile");
+});
+
+// @desc get users
+// @route GET /api/users/
+// @access private/admin
+const getUsers = asyncHandler(async (req, res) => {
+  res.send("get  users");
+});
+
+// @desc get users by id
+// @route GET /api/users/:id
+// @access private/admin
+const getUserById = asyncHandler(async (req, res) => {
+  res.send("get  users");
+});
+
+// @desc delete users
+// @route DELETE /api/users/:id
+// @access private/admin
+const deleteUser = asyncHandler(async (req, res) => {
+  res.send("delete  user");
+});
+
+// @desc Update users
+// @route DELETE /api/users/:id
+// @access private/admin
+const updateUser = asyncHandler(async (req, res) => {
+  res.send("Update  user");
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUsers,
+  getUserById,
+  updateUser,
+};
