01328f58ef76cef21f26d5316b1a0d8e2479435a
RAKIB <rakibmahmudmridha@gmail.com>
2024-01-17 10:26:23 +0600

    fixed error

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -25,7 +25,7 @@ const authUser = asyncHandler(async (req, res) => {
 });
 
 // @desc Register user
-// @route POST /api/users/register
+// @route POST /api/users
 // @access public
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
@@ -40,7 +40,7 @@ const registerUser = asyncHandler(async (req, res) => {
   const user = await User.create({ name, email, password });
 
   if (user) {
-    generateToken(res, use._id);
+    generateToken(res, user._id);
     res.status(201).json({
       _id: user._id,
       name: user.name,
